589d25285b82f5da9c294b2736b15074f16e91db
Pedro Cunha <idk.pedroc@gmail.com>
2023-07-17 03:01:05 -0300

    add: resources to items and stores new line to dockerfile

---
+++ resources/item.py
@@ -0,0 +1,58 @@
+from db import items, stores
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from flask import request
+import uuid
+
+blb = Blueprint('items', __name__, description="Items operations")
+
+blb.route('/item/<item_id>/')
+class Item(MethodView):
+    def get(self, item_id):
+        try:
+            return items[item_id], 200
+        except KeyError:
+            abort(404,message="Item not found")
+
+    def delete(self, item_id):
+        try:
+            del items[item_id]
+            return {"message": "Item deleted"}
+        except KeyError:
+            abort(404,message="Item not found")
+    
+    def put(self, item_id):
+        item_data = request.get_json()
+        if "price" not in item_data or "name" not in item_data:
+            abort(400, message="Bad request. Ensure 'price' and 'name' are in JSON payload")
+        try:
+            item = items[item_id]
+            item |= item_data
+            return item
+        except KeyError:
+            abort(400, message="Item not found.")
+
+blb.route('/item/')
+class ItemList(MethodView):
+
+    def get(self):
+        return {"items": list(items.values())}, 200
+    
+    def post(self):
+        item_data = request.get_json()
+        if (
+            "price" not in item_data
+            or "store_id" not in item_data
+            or "name" not in item_data
+        ):
+            abort(400, message="Bad request, Ensure 'price', 'store_id' and 'name' are included in the JSON payload")
+        for item in items.values():
+            if (item_data["name"] == item["name"] 
+            and item_data["store_id"] == item["store_id"]):
+                abort(400, message="Item already exist")
+        
+        if item_data["store_id"] not in stores:
+            abort(404,message="Store not found")
+        item_id = uuid.uuid4().hex
+        item = {**item_data, "id": item_id}
+        items[item_id] = item
---
+++ resources/store.py
@@ -0,0 +1,39 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import stores
+
+blp = Blueprint("stores", __name__, description="Operations in stores")
+
+@blp.route('/store/<string:store_id>/')
+class Store(MethodView):
+    def get(self, store_id):
+        try:
+            return stores[store_id]
+        except KeyError:
+            abort(404,message="Store not found")
+
+    def delete(self, store_id):
+        try:
+            del stores[store_id]
+            return {"message": "Store deleted"}
+        except KeyError:
+            abort(404,message="Store not found")
+
+@blp.route('/store/')
+class StoreList(MethodView):
+    def get(self):
+        return {"stores": list(stores.values())}
+    
+    def post(self):
+        store_data = request.get_json()
+        if "name" not in store_data:
+            abort(400, message="Bad request. Ensure 'name' is included in JSON payload")
+        for store in stores.values():
+            if store["name"] == store_data["name"]:
+                abort(400, mesasge="Store already exist")
+        store_id = uuid.uuid4().hex
+        new_store = {**store_data, "id": store_id}
+        stores[store_id] = new_store
+        return new_store, 201
